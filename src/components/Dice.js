c1442f4a6a31ab4538017aad5ca765e242088906
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-28 10:31:49 +0300

    added a functon

---
+++ src/components/Dice.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./Dice.css";
+
+const DiceGame = () => {
+    const [num, setNum] = useState(0);
+    const faces = ['1', '2', '3', '4', '5', '6'];
+  
+}
+  
+  export default DiceGame;
